a6cecdce3884a501bdd9026201401831fbfdb3d0
acsearles <acsearles@gmail.com>
2016-05-24 10:03:40 -0500

    MONA-143 Load initial files

---
+++ README.md
@@ -0,0 +1,31 @@
+Vars to be replaced:
+ - iui-basic
+ - iuiBasic
+ - iui.basicTemplates
+ - iui.basic
+ - Basic
+ - basic
+
+You will then need to change all of the file and folder names with `basic` and change them to the desired directive / module name
+
+# iui-basic
+
+*Angular version: 1.3.2*
+
+Follow the Angular Style Guide:
+https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md
+
+## Scripts
+- `npm start` - alias for `npm run server`
+- `npm run build` - compiles/minfies JS & CSS
+- `npm run test` - runs linting and unit test
+- `npm run test-continuous` - runs linting and unit test continuously
+- `npm run server` - creates a small web server - useful for building
+
+## Build
+**Note:** if you make changes, you must run `npm run build` to rebuild the combined files in the 'dist' directory. You will need to also update the package.json version number.
+
+## Changelog
+
+### 1.0.0
+- initial creation of iui-basic directive repo
---
+++ karma.conf.js
@@ -0,0 +1,94 @@
+module.exports = function (config) {
+  'use strict';
+  config.set({
+    basePath: '',
+
+    port: 9267,
+
+    runnerPort: 9100,
+
+    colors: true,
+
+    autoWatch: true,
+
+    logLevel: config.LOG_INFO,
+
+    browsers: ['PhantomJS'],
+
+    captureTimeout: 60000,
+
+    browserDisconnectTolerance: 0,
+
+    browserNoActivityTimeout: 60000,
+
+    singleRun: true,
+
+    plugins: [
+      'karma-jasmine',
+      'karma-phantomjs-launcher',
+      'karma-coverage',
+      'karma-babel-preprocessor',
+      'karma-ng-html2js-preprocessor',
+      'karma-spec-reporter'
+    ],
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+
+    htmlReporter: {
+      outputDir: 'karma_html',
+      templatePath: __dirname + '/node_modules/karma-html-reporter/jasmine_template.html'
+    },
+
+    files: [
+      // app core dependencies
+      'node_modules/jquery/dist/jquery.js',
+      'test/test-helper.js',
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+
+      // app module dependencies
+      'lib/src/**/*.js',
+
+      //directive templates
+      'lib/src/**/*.html',
+      'lib/src/**/*.svg',
+      // test scripts
+      'test/**/*.test.js'
+    ],
+
+    reporters: ['coverage', 'progress', 'html', 'spec'],
+
+    preprocessors: {
+      'lib/src/**/*.js' : ['babel'],
+      'test/**/*.test.js' : ['babel'],
+      'lib/src/**/*.html': ['ng-html2js'],
+      'lib/src/**/*.svg': ['ng-html2js']
+    },
+
+    ngHtml2JsPreprocessor: {
+      // immitates ui-core
+      prependPrefix: '/$iui-basic',
+      stripPrefix: 'lib/src',
+      moduleName: 'templates'
+    },
+
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015'],
+        sourceMap: 'inline'
+      },
+      filename: function (file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function (file) {
+        return file.originalPath;
+      }
+    },
+
+    frameworks: ['jasmine']
+  });
+};
